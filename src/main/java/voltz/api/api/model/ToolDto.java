e5bc03e6b6044493628e5097ce3b94d571998d1b
Deivid <dev.deividcorrea@gmail.com>
2022-12-28 17:08:32 -0300

    Ajustes Dto

---
+++ src/main/java/voltz/api/api/model/ToolDto.java
@@ -0,0 +1,25 @@
+package voltz.api.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToolDto {
+
+    private String title;
+    private String link;
+    @Length(max = 256, message = "Campo deve conter no maximo 256 carcateres")
+    private String description;
+    @Size(min = 0, max = 8, message = "Campo deve conter no maximo 8 tags")
+    private List<String> tags;
+
+}
