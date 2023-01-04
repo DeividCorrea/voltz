38398e84105f8565acd449ee07a6cd08a2347457
DeividCorrea <121562604+DeividCorrea@users.noreply.github.com>
2023-01-04 09:39:20 -0300

    Update and rename ToolDto.java to ToolRequestDto.java

--- src/main/java/voltz/api/api/model/ToolRequestDto.java
+++ src/main/java/voltz/api/api/model/ToolRequestDto.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ToolDto {
+public class ToolRequestDto {
 
     private String title;
     private String link;
